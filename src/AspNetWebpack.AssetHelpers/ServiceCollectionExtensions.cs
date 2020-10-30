1fb0096742c3218c7f0937abd80fbda5d5424b82
Morten Larsen <baunegaard@live.dk>
2020-10-30 01:58:14 +0100

    Add ServiceCollectionExtensions

---
+++ src/AspNetWebpack.AssetHelpers/ServiceCollectionExtensions.cs
@@ -0,0 +1,45 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace AspNetWebpack.AssetHelpers
+{
+    /// <summary>
+    /// Extensions methods for IServiceCollection.
+    /// </summary>
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds AssetService and necessary dependencies.
+        /// </summary>
+        /// <param name="services">The service collection.</param>
+        /// <param name="configuration">The configuration.</param>
+        /// <param name="env">The hosting environment.</param>
+        /// <returns>The modified service collection.</returns>
+        /// <exception cref="ArgumentNullException">If services or configuration is null.</exception>
+        public static IServiceCollection AddAssetHelpers(this IServiceCollection services, IConfiguration configuration, IWebHostEnvironment env)
+        {
+            if (services == null)
+            {
+                throw new ArgumentNullException(nameof(services));
+            }
+
+            if (configuration == null)
+            {
+                throw new ArgumentNullException(nameof(configuration));
+            }
+
+            if (env.IsDevelopment())
+            {
+                services.AddHttpClient();
+            }
+
+            services.Configure<WebpackOptions>(configuration.GetSection("Webpack"));
+            services.AddSingleton<IAssetService, AssetService>();
+
+            return services;
+        }
+    }
+}
