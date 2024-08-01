057ea95ce0d2e037f9400373e2db398becc33d2e
Morten Larsen <baunegaard@live.dk>
2024-08-01 23:58:24 +0200

    Upgrade build project

--- build/Build.cs
+++ build/Build.cs
@@ -1,75 +1,74 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using Nuke.Common;
 using Nuke.Common.CI;
-using Nuke.Common.Execution;
+using Nuke.Common.CI.AppVeyor;
+using Nuke.Common.Git;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
+using Nuke.Common.Tooling;
+using Nuke.Common.Tools.Codecov;
 using Nuke.Common.Tools.Coverlet;
 using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Tools.GitVersion;
+using Nuke.Common.Tools.NuGet;
 using Nuke.Common.Tools.ReportGenerator;
 using Nuke.Common.Utilities.Collections;
-using static Nuke.Common.IO.FileSystemTasks;
+using static Nuke.Common.Tools.Codecov.CodecovTasks;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
+using static Nuke.Common.Tools.NuGet.NuGetTasks;
+using static Nuke.Common.Tools.ReportGenerator.ReportGeneratorTasks;
 
-// ReSharper disable InconsistentNaming
-// ReSharper disable UnusedMember.Local
+// ReSharper disable AllUnderscoreLocalParameterName
 
-[CheckBuildProjectConfigurations]
 [ShutdownDotNetAfterServerBuild]
+// [AppVeyor(
+//     AppVeyorImage.UbuntuLatest,
+//     InvokedTargets =
+//     [
+//         nameof(UploadCodecov),
+//         nameof(PushNuGet),
+//         nameof(PushMyGet),
+//     ])]
+[AppVeyorSecret("MYGET_API_KEY", "78qy8e6pKfJlQV7RAG5tJOWegzXpjASkUs3aFdVBoPYA5gi6+mWdjbuAmNa5OQPe")]
+[AppVeyorSecret("NUGET_API_KEY", "6MkPTvHJIpZhqOH9BCycM4BA++x1SYTINJQ04uR+BxZn2UaUmatB7NkwPCVGBviq")]
 class Build : NukeBuild
 {
-    /// Support plugins are available for:
-    ///   - JetBrains ReSharper        https://nuke.build/resharper
-    ///   - JetBrains Rider            https://nuke.build/rider
-    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
-    ///   - Microsoft VSCode           https://nuke.build/vscode
-
-    static readonly AbsolutePath SourceDirectory = RootDirectory / "src";
-    static readonly AbsolutePath ArtifactsDirectory = RootDirectory / "artifacts";
-    static readonly AbsolutePath CoverageDirectory = RootDirectory / "coverage";
+    public static int Main () => Execute<Build>(x => x.Compile);
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
-    [Parameter] readonly bool APPVEYOR_REPO_TAG;
-    [Parameter] readonly string APPVEYOR_REPO_BRANCH;
-    [Parameter] readonly string MYGET_API_KEY;
-    [Parameter] readonly string NUGET_API_KEY;
+    [Parameter(Name = "MYGET_API_KEY")] [Secret] string MyGetApiKey { get; set; }
+    [Parameter(Name = "NUGET_API_KEY")] [Secret] string NuGetApiKey { get; set; }
 
     [Solution] readonly Solution Solution;
+    [GitRepository] readonly GitRepository GitRepository;
+    [GitVersion] readonly GitVersion GitVersion;
+    [CI] readonly AppVeyor AppVeyor;
 
-    GitVersion GitVersion;
+    static AbsolutePath SourceDirectory => RootDirectory / "src";
+    static AbsolutePath ArtifactsDirectory => RootDirectory / "artifacts";
 
-    public static int Main() => Execute<Build>(x => x.Pack);
+    static AbsolutePath CoverageDirectory => RootDirectory / "coverage";
+
+    static AbsolutePath CoverageResult => CoverageDirectory / "Cobertura.xml";
+
+    static IEnumerable<AbsolutePath> Artifacts => ArtifactsDirectory.GlobFiles("*.nupkg");
 
     Target Clean => _ => _
-        .Before(Version)
+        .Before(Compile)
         .Executes(() =>
         {
-            SourceDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
-            EnsureCleanDirectory(ArtifactsDirectory);
-        });
+            SourceDirectory
+                .GlobDirectories("**/bin", "**/obj")
+                .ForEach(path => path.DeleteDirectory());
 
-    Target Version => _ => _
-        .Executes(() =>
-        {
-            GitVersion = GitVersionTasks.GitVersion(s => s
-                .SetFramework("netcoreapp3.1")
-                .SetUpdateAssemblyInfo(!IsLocalBuild)
-                .EnableNoFetch())
-                .Result;
-        });
-
-    Target Restore => _ => _
-        .DependsOn(Version)
-        .Executes(() =>
-        {
-            DotNetRestore(s => s
-                .SetProjectFile(Solution));
+            ArtifactsDirectory.CreateOrCleanDirectory();
         });
 
     Target Compile => _ => _
-        .DependsOn(Restore)
         .Executes(() =>
         {
             DotNetBuild(s => s
@@ -78,95 +77,96 @@ class Build : NukeBuild
                 .SetAssemblyVersion(GitVersion.AssemblySemVer)
                 .SetFileVersion(GitVersion.AssemblySemFileVer)
                 .SetInformationalVersion(GitVersion.InformationalVersion)
-                .EnableTreatWarningsAsErrors()
-                .EnableNoRestore());
+                .EnableTreatWarningsAsErrors());
         });
 
     Target Test => _ => _
         .DependsOn(Compile)
         .Executes(() =>
         {
-            foreach (var project in Solution.GetProjects("*.Tests"))
+            CoverageDirectory.CreateOrCleanDirectory();
+            TestResultFolders.ForEach(directory => directory.DeleteDirectory());
+
+            DotNetTest(s => s
+                .SetConfiguration(Configuration)
+                .EnableNoRestore()
+                .EnableNoBuild()
+                .SetDataCollector("XPlat Code Coverage")
+                .SetCoverletOutputFormat(CoverletOutputFormat.cobertura)
+                .CombineWith(TestProjects, (ss, project) => ss
+                    .SetProjectFile(project)),
+                degreeOfParallelism: Environment.ProcessorCount);
+
+            ReportGenerator(s => s
+                .SetReports(CoverageResults)
+                .SetTargetDirectory(CoverageDirectory)
+                .SetReportTypes(ReportTypes.Cobertura));
+
+            if (IsLocalBuild)
             {
-                var settings = new DotNetTestSettings()
-                    .SetProjectFile(project)
-                    .SetConfiguration(Configuration)
-                    .EnableNoRestore()
-                    .EnableNoBuild();
-
-                var outDir = project.GetMSBuildProject(Configuration).GetPropertyValue("OutputPath");
-                var assembly = project.Directory / outDir / $"{project.Name}.dll";
-
-                EnsureCleanDirectory(CoverageDirectory);
-
-                CoverletTasks.Coverlet(s => s
-                    .SetTargetSettings(settings)
-                    .SetAssembly(assembly)
-                    .SetOutput(CoverageDirectory / "coverage")
-                    .SetFormat(CoverletOutputFormat.opencover)
-                    .AddExclude("[AspNetWebpack.Testing]*"));
-
-                ReportGeneratorTasks.ReportGenerator(s => s
-                    .SetFramework("netcoreapp3.0")
-                    .SetTargetDirectory(CoverageDirectory)
-                    .SetReports(CoverageDirectory / "coverage.opencover.xml"));
+                ReportGenerator(s => s
+                    .SetReports(CoverageResult)
+                    .SetTargetDirectory(CoverageDirectory));
             }
         });
 
-    Target Pack => _ => _
+    Target Package => _ => _
         .DependsOn(Test)
         .Produces(ArtifactsDirectory / "*.nupkg")
         .Executes(() =>
         {
-            EnsureCleanDirectory(ArtifactsDirectory);
+            ArtifactsDirectory.CreateOrCleanDirectory();
 
             DotNetPack(s => s
-                .SetProject(Solution)
                 .SetConfiguration(Configuration)
-                .SetOutputDirectory(ArtifactsDirectory)
-                .SetVersion(GitVersion.NuGetVersionV2)
-                .EnableIncludeSource()
-                .EnableIncludeSymbols()
-                .SetSymbolPackageFormat(DotNetSymbolPackageFormat.snupkg)
                 .EnableNoRestore()
-                .EnableNoBuild());
+                .EnableNoBuild()
+                .SetOutputDirectory(ArtifactsDirectory)
+                .SetVersion(GitVersion.SemVer));
         });
 
-    Target Push => _ => _
-        .DependsOn(Pack)
+    Target PushMyGet => _ => _
+        .DependsOn(Package)
+        .OnlyWhenStatic(() => IsServerBuild && GitRepository.IsOnMainBranch())
         .Executes(() =>
         {
-            if (APPVEYOR_REPO_TAG)
-            {
-                ArtifactsDirectory.GlobFiles("*.nupkg")
-                    .ForEach(x =>
-                    {
-                        DotNetNuGetPush(s => s
-                            .SetTargetPath(x)
-                            .SetSource("https://api.nuget.org/v3/index.json")
-                            .SetApiKey(NUGET_API_KEY));
-                    });
-            }
-
-            if (APPVEYOR_REPO_BRANCH == "main")
-            {
-                ArtifactsDirectory.GlobFiles("*.nupkg")
-                    .ForEach(x =>
-                    {
-                        DotNetNuGetPush(s => s
-                            .SetTargetPath(x)
-                            .SetSource("https://www.myget.org/F/baunegaard/api/v2/package")
-                            .SetApiKey(MYGET_API_KEY));
-                    });
-
-                ArtifactsDirectory.GlobFiles("*.snupkg")
-                    .ForEach(x =>
-                    {
-                        DotNetNuGetPush(s => s
-                            .SetTargetPath(x)
-                            .SetSource("https://www.myget.org/F/baunegaard/api/v3/index.json")
-                            .SetApiKey(MYGET_API_KEY));
-                    });
-            }
+            NuGetPush(s => s
+                .SetSource("https://www.myget.org/F/baunegaard/api/v2/package")
+                .SetApiKey(MyGetApiKey)
+                .CombineWith(Artifacts, (ss, artifact) => ss
+                    .SetTargetPath(artifact)),
+                degreeOfParallelism: Environment.ProcessorCount);
         });
+
+    Target PushNuGet => _ => _
+        .DependsOn(Package)
+        .OnlyWhenStatic(() => IsServerBuild && AppVeyor.RepositoryTag)
+        .Executes(() =>
+        {
+            NuGetPush(s => s
+                .SetSource("https://api.nuget.org/v3/index.json")
+                .SetApiKey(NuGetApiKey)
+                .CombineWith(Artifacts, (ss, artifact) => ss
+                    .SetTargetPath(artifact)),
+                degreeOfParallelism: Environment.ProcessorCount);
+        });
+
+    Target UploadCodecov => _ => _
+        .DependsOn(Test)
+        .OnlyWhenStatic(() => IsServerBuild)
+        .Executes(() =>
+        {
+            Codecov(s => s
+                .SetFiles(CoverageResult));
+        });
+
+    IEnumerable<Project> TestProjects => Solution.GetAllProjects("*.Tests");
+
+    IEnumerable<AbsolutePath> TestResultFolders => TestProjects
+        .SelectMany(project => project.Directory.GlobDirectories("TestResults"));
+
+    IEnumerable<string> CoverageResults => TestResultFolders
+        .SelectMany(testResults => testResults.GlobDirectories("*"))
+        .SelectMany(output => output.GlobFiles("coverage.cobertura.xml"))
+        .Select(file => file.ToString());
 }
--- build/Configuration.cs
+++ build/Configuration.cs
@@ -4,9 +4,8 @@ using Nuke.Common.Tooling;
 [TypeConverter(typeof(TypeConverter<Configuration>))]
 public class Configuration : Enumeration
 {
-    public static Configuration Debug { get; } = new() { Value = nameof(Debug) };
-
-    public static Configuration Release { get; } = new() { Value = nameof(Release) };
+    public static Configuration Debug = new() { Value = nameof(Debug) };
+    public static Configuration Release = new() { Value = nameof(Release) };
 
     public static implicit operator string(Configuration configuration)
     {
